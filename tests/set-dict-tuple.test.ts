e83913e2f6d8709f0d7784b63251896e8ae2f060
Yifei <yifeiz@Yifeis-MacBook-Pro.local>
2022-05-12 14:36:22 -0700

    NEW TESTs

---
+++ tests/set-dict-tuple.test.ts
@@ -0,0 +1,71 @@
+import { assertTC, assertTCFail, assertPrint } from './asserts.test';
+import { NUM, BOOL, NONE, TUPLE, SET, DICT } from '../utils';
+import {expect} from 'chai';
+import { parser } from 'lezer-python';
+import { traverseExpr, traverseStmt, traverse, parse } from '../parser';
+
+describe('parse(source) function', () => {
+    // TODO: add additional tests here to ensure parse works as expected
+    // Week6: Project Milestone Tests
+    it("parse an empty dict expression", () => {
+      const parsed = parse("d = {}");
+      expect(parsed.stmts).to.deep.equal([
+        { tag: "assign", name: "d", value: {tag: "dict_expr", entries: [] }}]);
+    });
+  
+    it ('parse a set', () =>{
+      const parsed = parse("s = {34,False,None}");
+      expect(parsed.stmts).to.deep.equal([
+        {tag: "assign", name: "s", value: {tag: "set_expr", contents: 
+            [{tag: "literal", value: {tag:"num", value:34}},{tag: "literal", value: {tag:"bool", value:false}}, {tag: "literal", value: {tag:"none"}}] }}]);
+    });
+    
+    it ('parse a tuple', () =>{
+      const parsed = parse("t = (34,True,None)");
+      expect(parsed.stmts).to.deep.equal([
+        {tag: "assign", name: "t", value:{tag: "tuple_expr", contents: 
+        [{tag: "literal", value: {tag:"num", value:34}},{tag: "literal", value: {tag:"bool", value:true}}, {tag: "literal", value: {tag:"none"}}] }}]);
+    });
+   
+  });
+
+
+// Week6: Project Milestone Tests
+// describe("proj-tuple-test", ()=>{
+  
+//   assertTC("tc-assign-tuple-mixed",`
+//   x:tuple = (3,False,None)
+//   x`,TUPLE([NUM,BOOL,NONE]));
+
+//   assertPrint("pr-tuple-indexing",`
+//   x:tuple = (3,False,None)
+//   print(x[0])
+//   print(x[1])
+//   print(x[2])`,["3","False","None"])
+// });
+  
+describe("proj-set-test", ()=>{
+
+  assertTC("tc-assign-set-int",`
+  x:set = {3,76,5}
+  x`, SET(NUM));
+    
+  // assertPrint("pr-set-add-dupl",`
+  // x:set = {1,2}
+  // x.add(1)
+  // print(x)`,["{1,2}"])
+});
+     
+// describe("proj-dict-test", ()=>{
+    
+//   assertTC("tc-assign-dict-int-bool",`
+//   x:set = {1:True,2:False}`, DICT(NUM,BOOL))
+
+//   assertPrint("pr-dict-int-bool-key",`
+//   x:dict = {1:True,2:False}
+//   print(x[1])
+//   x[1] = False
+//   print(x[1])
+//   x[1] = False
+//   print(x[1])`,["True","False"])
+// });
