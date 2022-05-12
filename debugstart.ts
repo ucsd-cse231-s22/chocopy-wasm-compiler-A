1c3fb23af50e5e25e660345a3d37ac9071fde989
tongyanjun <yj.sjtu@gmail.com>
2022-05-12 14:49:21 -0700

    fix design example

--- debugstart.ts
+++ debugstart.ts
@@ -6,13 +6,28 @@ import { importObject, addLibs  } from "./tests/import-object.test";
 // entry point for debugging
 async function debug() {
   var source = `
-idx : int = 0
-
-x : [int] = None
-x = [1,3,5,7,9]+[2,4,6,8+9,10]
-x[2*4]
-  
+def f() -> int:
+  x : int = 0
+  for x in [1, 2, 3, 4, 5]:
+    if x > 2:
+      return x
+    else:
+      pass
+  return x
+print(f())
 `
+  var source2 = `
+def f() -> int:
+  x : int = 1
+  while x<100:
+    if x > 3:
+      return x
+    else:
+      pass
+    x = x+1
+  return x
+print(f())
+  `
 // a comprehensive test that tests all features of list
 // 1. list assignment(entries can be expr)
 // 2. list index(index can be expr)
@@ -30,7 +45,7 @@ a[2] = 5
 c = a+b
 print(c[2*3+1]) `
 
-  const ast = parse(source);
+  // const ast = parse(source);
   
   const repl = new BasicREPL(await addLibs());
   const result = repl.run(source).then(result => {
