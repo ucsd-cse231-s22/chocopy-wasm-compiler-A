5fc3bf7b3ef97a06666290dfee25941668db6d3b
Zhiwei <sc16zr@leeds.ac.uk>
2022-05-13 01:35:06 -0700

    strTC

--- tests/pa3-visible.test.ts
+++ tests/pa3-visible.test.ts
@@ -9,4 +9,19 @@ describe("PA3 visible tests", () => {
   assertTCFail("string - int",`
   x:str = None
   x = 1`);
+  //2
+  assertPrint("String equals True 1",`
+  print("abcd" == "abcd")
+  `,['True'])
+  assertPrint("String equals False 1",`
+  x:str = None
+  y:str = None
+  x = "abcd"
+  y = "bcde"
+  print(x == y)
+  `,['False'])
+  //3
+  assertPrint("String equals False 2",`
+  print("abcd" == "bcde")
+  `,['False'])
 });
