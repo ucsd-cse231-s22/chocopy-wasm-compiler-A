49d6658eab36dbba10249a834df1ca41d70dd7cc
Venkat Krishnamohan <vk@Venkats-MacBook-Air.local>
2022-05-12 20:37:38 -0700

    added pred and succ lists

--- optimization_utils.ts
+++ optimization_utils.ts
@@ -12,3 +12,18 @@ export function isTagNone(value: Value<Type>): value is { tag: "none", a?: Type}
 export function isTagBoolean(value: Value<Type>): value is { tag: "bool", value: boolean, a?: Type} {
     return value.tag === "bool";
 }
+
+export function isTagBigInt(value: Value<Type>): value is { tag: "num", value: bigint, a?: Type} {
+    return value.tag === "num";
+}
+
+export function isTagNumber(value: Value<Type>): value is { tag: "wasmint", value: number, a?: Type} {
+    return value.tag === "wasmint";
+}
+
+export function isTagEqual(a: Value<Type>, b: Value<Type>): boolean {
+    if(isTagBigInt(a) && isTagBigInt(b) || isTagBoolean(a) && isTagBoolean(b) || isTagNone(a) && isTagNone(b))
+        return true
+    else
+        return false
+}
