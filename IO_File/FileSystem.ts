3a9a4a0f3548401119efb3a07533a1e6bf9e5c30
Qi <qizhang5@cs.stonybrook.edu>
2022-05-12 21:26:24 -0700

    read and open in FileSystem.ts

--- IO_File/FileSystem.ts
+++ IO_File/FileSystem.ts
@@ -6,27 +6,35 @@
 // What is the type for this open File
 export type OpenFile = {
     currentPosition: number,
-    dataArray: Array<Byte> 
-} 
+    dataArray: Array<number>
+}
 
-const buildin_file_libs = ```
+const buildin_file_libs = `
 (func $buildin_open (import "libmemory" "open") (param i32) (param i32) (param i32))
-```
+`;
 
 let fdCounter = 0;
 let fs = new Map<number, OpenFile>();
 
 /*
- * The input to open should be some value for string
+ * TODO Later: The input to open should be some value for string
+ * right now pretend that we are creating a file without a fname string
  */
 export function open(addr: number): number {
-    const dummyFile = "./test.txt";
-
-    return -1;
+    fs.set(fdCounter++, {
+        currentPosition: 0,
+        dataArray: []
+    });
+    return fdCounter - 1;
 }
 
 export function read(fd: number): number {
+    if (fs.has(fd)) {
+        let currPos = fs.get(fd).currentPosition;
+        return fs.get(fd).dataArray[currPos];
+    };
     return -1;
+
 }
 
 export function write(fd: number): number {
@@ -40,7 +48,7 @@ export function close(fd: number) {
 
 export function seek(fd: number, pos: number) {
 
-} 
+}
 
 
 // read(file_name, mode="read string") -> string
