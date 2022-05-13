31b2e0994066b3ec23d008c42e4f92d8b8c929d3
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-05-13 13:51:10 -0700

    TODOs left for type errors: tcStmt and tcExpr

---
+++ debugstart.ts
@@ -0,0 +1,28 @@
+import { parse } from "./parser";
+import { BasicREPL } from "./repl";
+import { importObject, addLibs  } from "./tests/import-object.test";
+
+
+// entry point for debugging
+async function debug() {
+//   var source = `
+// class C(object):
+//   def f(self: C) -> int:
+//     if True:
+//       return 0
+//     else:
+//       return`
+  var source = `
+class C(object):
+  def __init__(self: C, a: D):
+    pass`
+const ast = parse(source);
+  
+  const repl = new BasicREPL(await addLibs());
+  const result = repl.run(source).then(result => {
+    console.log(result);    
+  })  
+}
+
+debug();
+
