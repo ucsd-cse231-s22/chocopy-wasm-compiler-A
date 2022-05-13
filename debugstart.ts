671066a1832a13f0ccd864a6d2fd1f9eca9d7eaf
Utkarsh Vashishtha <utkarsh.vashishtha1997@gmail.com>
2022-05-12 20:41:11 -0700

    traverse over the entire AST

--- debugstart.ts
+++ debugstart.ts
@@ -12,144 +12,27 @@ async function debug() {
 
   class Box(Generic[T]):
       f : T = __ZERO__
-      a : Box[T] = __ZERO__
+      a : Box[T] = None
 
       def getF(self: Box[T]) -> T:
           return self.f
 
       def setF(self: Box[T], f: T):
+          print(self.f)
           self.f = f
+          print(self.f)
 
   b : Box[int] = None
-  b = Box()`
+  b = Box()
+  print(b.getF())`
   const ast = parse(source);
-  const util = require('util')
-  console.log(util.inspect(ast, false, null, true /* enable colors */)) 
-
-  const tast = {
-    funs: [] as Array<FunDef<Type>>,
-    inits: [
-      {
-        name: 'b',
-        type: { tag: 'class', name: 'Box', params: [ { tag: 'number' } ] },
-        value: { tag: 'none' }
-      }
-    ],
-    typeVarInits: [
-      {
-        name: 'T',
-        canonicalName: 'T',
-        types: [ { tag: 'number' }, { tag: 'bool' } ]
-      }
-    ],
-    classes: [
-      {
-        name: 'Box',
-        typeParams: [ 'T' ],
-        fields: [
-          {
-            name: 'f',
-            type: { tag: 'typevar', name: 'T' },
-            value: { tag: 'zero' }
-          },
-          {
-            name: 'a',
-            type: {
-              tag: 'class',
-              name: 'Box',
-              params: [ { tag: 'typevar', name: 'T' } ]
-            },
-            value: { tag: 'zero' }
-          }
-        ],
-        methods: [
-          {
-            name: 'getF',
-            parameters: [
-              {
-                name: 'self',
-                type: {
-                  tag: 'class',
-                  name: 'Box',
-                  params: [ { tag: 'typevar', name: 'T' } ]
-                }
-              }
-            ],
-            ret: { tag: 'typevar', name: 'T' },
-            inits: [] as Array<VarInit<Type>>,
-            body: [
-              {
-                tag: 'return',
-                value: {
-                  tag: 'lookup',
-                  obj: { tag: 'id', name: 'self' },
-                  field: 'f'
-                }
-              }
-            ]
-          },
-          {
-            name: 'setF',
-            parameters: [
-              {
-                name: 'self',
-                type: {
-                  tag: 'class',
-                  name: 'Box',
-                  params: [ { tag: 'typevar', name: 'T' } ]
-                }
-              },
-              {
-                name: 'f',
-                type: { tag: 'typevar', name: 'T' }
-              }
-            ],
-            ret: { tag: 'none' },
-            inits: [] as Array<VarInit<Type>>,
-            body: [
-              {
-                tag: 'field-assign',
-                obj: { tag: 'id', name: 'self' },
-                field: 'f',
-                value: { tag: 'id', name: 'f' }
-              }
-            ]
-          },
-          {
-            name: '__init__',
-            parameters: [
-              {
-                name: 'self',
-                type: {
-                  tag: 'class',
-                  name: 'Box',
-                  params: [ { tag: 'typevar', name: 'T' } ]
-                }
-              }
-            ],
-            ret: { tag: 'none' },
-            inits: [],
-            body: []
-          }
-        ]
-      }
-    ],
-    stmts: [
-      {
-        tag: 'assign',
-        name: 'b',
-        value: { tag: 'call', name: 'Box', arguments: [] as Array<Parameter<Type>> }
-      }
-    ]
-  }
-
-  const mast = monomorphizeProgram(tast as Program<Type>);
-  console.log(util.inspect(mast, false, null, true /* enable colors */)) 
+  // const util = require('util')
+  // console.log(util.inspect(ast, false, null, true /* enable colors */))  
   
-  // const repl = new BasicREPL(await addLibs());
-  // const result = repl.run(source).then(result => {
-  //   console.log(result);    
-  // })  
+  const repl = new BasicREPL(await addLibs());
+  const result = repl.run(source).then(result => {
+    console.log(result);    
+  })  
 }
 
 debug();
--- monomorphizer.ts
+++ monomorphizer.ts
@@ -1,4 +1,4 @@
-import { Class, Literal, Parameter, Program, Type, VarInit } from './ast';
+import { Class, Expr, Literal, Parameter, Program, Stmt, Type, VarInit } from './ast';
 import { CLASS } from './utils';
 
 export type GlobalMorphEnv = {
@@ -27,6 +27,133 @@ export function resolveZero(type: Type) : Literal {
             return { tag: "bool", value: false };
         case "class":
             return { tag: "none" };
+    }
+}
+
+export function processExprs(expr: Expr<Type>, genv: GlobalMorphEnv) : Expr<Type> {
+    expr.a = concretizeGenericTypes(expr.a, genv);
+    switch(expr.tag) {
+        case "binop":
+            const binL = processExprs(expr.left, genv);
+            const binR = processExprs(expr.right, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), left: binL, right: binR };
+            }
+            return { ...expr, left: binL, right: binR };
+        case "call":
+            const cExprs = expr.arguments.map(a => processExprs(a, genv));
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), arguments: cExprs };
+            }
+            return { ...expr, arguments: cExprs };
+        case "construct":
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                const constructCname = getCanonicalTypeName(expr.a)
+                return { ...expr, a: CLASS(constructCname), name: constructCname };
+            }
+            return expr;
+        case "id":
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)) };
+            }
+            return expr;
+        case "index":
+            const inxExpr = processExprs(expr.index, genv);
+            const inxObj = processExprs(expr.obj, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), index: inxExpr, obj: inxObj };
+            }
+            return { ...expr, index: inxExpr, obj: inxObj };
+        case "lookup":
+            const lObj = processExprs(expr.obj, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), obj: lObj };
+            }
+            return { ...expr, obj: lObj };
+        case "method-call":
+            const mcExprs = expr.arguments.map(a => processExprs(a, genv));
+            const mcObj = processExprs(expr.obj, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), arguments: mcExprs, obj: mcObj };
+            }
+            return { ...expr, arguments: mcExprs, obj: mcObj };
+        case "uniop":
+            const uexpr = processExprs(expr.expr, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), expr: uexpr };
+            }
+            return { ...expr, expr: uexpr };
+        case "builtin1":
+            const b1arg = processExprs(expr.arg, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), arg: b1arg };
+            }
+            return { ...expr, arg: b1arg };
+        case "builtin2":
+            const b2left = processExprs(expr.left, genv);
+            const b2right = processExprs(expr.right, genv);
+            if (expr.a.tag === "class" && expr.a.params.length > 0) {
+                return { ...expr, a: CLASS(getCanonicalTypeName(expr.a)), left: b2left, right: b2right };
+            }
+            return { ...expr, left: b2left, right: b2right };
+        default:
+            return expr;
+    }
+}
+
+export function processStmts(stmt: Stmt<Type>, genv: GlobalMorphEnv) : Stmt<Type> {
+    stmt.a = concretizeGenericTypes(stmt.a, genv);
+    switch(stmt.tag) {
+        case "assign":
+            const assignValueExpr = processExprs(stmt.value, genv);
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return { a: CLASS(getCanonicalTypeName(stmt.a)), ...stmt, value: assignValueExpr };
+            }
+            return { ...stmt, value: assignValueExpr };
+        case "expr":
+            const expr = processExprs(stmt.expr, genv);
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return {...stmt, a: CLASS(getCanonicalTypeName(stmt.a)), expr };
+            }
+            return { ...stmt, expr };
+        case "field-assign":
+            const faobj = processExprs(stmt.obj, genv);
+            const faval = processExprs(stmt.value, genv);
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return { ...stmt, a: CLASS(getCanonicalTypeName(stmt.a)), obj: faobj, value: faval };
+            }
+            return { ...stmt, obj: faobj, value: faval };
+        case "index-assign":
+            const iaobj = processExprs(stmt.obj, genv);
+            const iinx = processExprs(stmt.index, genv);
+            const ival = processExprs(stmt.value, genv);
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return { ...stmt, a: CLASS(getCanonicalTypeName(stmt.a)), obj: iaobj, index: iinx, value: ival };
+            }
+            return { ...stmt, obj: iaobj, index: iinx, value: ival };
+        case "if":
+            const ifcond = processExprs(stmt.cond, genv);
+            const ifthn = stmt.thn.map(st => processStmts(st, genv));
+            const ifels = stmt.els.map(st => processStmts(st, genv));
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return { ...stmt, a: CLASS(getCanonicalTypeName(stmt.a)), cond: ifcond, thn: ifthn, els: ifels };
+            }
+            return { ...stmt, cond: ifcond, thn: ifthn, els: ifels };
+        case "return":
+            const retExpr = processExprs(stmt.value, genv);
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return { ...stmt, a: CLASS(getCanonicalTypeName(stmt.a)), value: retExpr };
+            }
+            return { ...stmt, value: retExpr };
+        case "while":
+            const wcond = processExprs(stmt.cond, genv);
+            const wBody = stmt.body.map(st => processStmts(st, genv));
+            if (stmt.a.tag === "class" && stmt.a.params.length > 0) {
+                return { ...stmt, a: CLASS(getCanonicalTypeName(stmt.a)), cond: wcond, body: wBody };
+            }
+            return { ...stmt, cond: wcond, body: wBody };
+        default:
+            return stmt;
     }
 }
 
@@ -59,7 +186,9 @@ export function monomorphizeClass(cname: string, canonicalName: string, classes:
 
     mClass.fields = processInits(mClass.fields, classes, genv);
     mClass.methods = mClass.methods.map(m => {
-        return { ...m, parameters: processMethodParams(m.parameters, classes, genv), inits: processInits(m.inits, classes, genv) };
+        // assuming method annotation is none, we don't do anything with it
+        return { ...m, parameters: processMethodParams(m.parameters, classes, genv), inits: processInits(m.inits, classes, genv), 
+                body: m.body.map(s => processStmts(s, genv)) };
     });
 
     return mClass;
@@ -116,5 +245,6 @@ export function monomorphizeProgram(program: Program<Type>) : Program<Type> {
     let genv : GlobalMorphEnv = {classesInx, typeVars: new Map(), morphedClasses: new Set()};
     const inits = processInits(program.inits, program.classes, genv);
     const monoMorphizedClasses = program.classes.filter(clazz => clazz.typeParams.length === 0);
-    return { ...program, inits, classes: monoMorphizedClasses, typeVarInits: [] };
+    const processedStmts = program.stmts.map(s => processStmts(s, genv));
+    return { ...program, inits, classes: monoMorphizedClasses, typeVarInits: [], stmts: processedStmts };
 }
