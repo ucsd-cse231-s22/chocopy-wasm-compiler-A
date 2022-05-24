e3059d2331b2c41888f76b867fd772073637897d
padmaja2023 <kollepadmaja23@gmail.com>
2022-05-23 23:23:23 -0700

    debugstart

--- debugstart.ts
+++ debugstart.ts
@@ -6,14 +6,14 @@ import { importObject, addLibs  } from "./tests/import-object.test";
 // entry point for debugging
 async function debug() {
   var source = `
-  class C(object):
+class C(object):
   def f(self: C) -> int:
     if True:
       return 0
     else:
       return`
   const ast = parse(source);
-  
+
   const repl = new BasicREPL(await addLibs());
   const result = repl.run(source).then(result => {
     console.log(result);    
